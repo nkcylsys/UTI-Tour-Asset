7811720ba0baf431817b2fb40405341ebcf89795
NK <nirmala.koli@cylsys.com>
2022-07-01 14:24:17 +0530

    UTI Tour Asset

---
+++ assets/js/message.js
@@ -0,0 +1,46 @@
+
+    //function myFunction() 
+      //document.getElementById("demo").innerHTML = "Your Request is Accepted";
+    //
+
+    function myFunctionA1() {
+      var checkBox = document.getElementById("myCheckA1");
+      var text = document.getElementById("texta1");
+      if (checkBox.checked == true){
+        text.style.display = "block";
+      } else {
+         text.style.display = "none";
+      }
+    }
+
+    function myFunctionS1() {
+        var checkBox = document.getElementById("myCheckS1");
+        var text = document.getElementById("texts1");
+        if (checkBox.checked == true){
+          text.style.display = "block";
+        } else {
+           text.style.display = "none";
+        }
+      }
+
+      function myFunctionA2() {
+        var checkBox = document.getElementById("myCheckA2");
+        var text = document.getElementById("texta2");
+        if (checkBox.checked == true){
+          text.style.display = "block";
+        } else {
+           text.style.display = "none";
+        }
+      }
+
+    function myFunctionS2() {
+      var checkBox = document.getElementById("myCheckS2");
+      var text = document.getElementById("texts2");
+      if (checkBox.checked == true){
+        text.style.display = "block";
+      } else {
+         text.style.display = "none";
+      }
+    }
+
+
